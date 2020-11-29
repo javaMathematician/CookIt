657a602daf4abcf98d10356d05807a5ebed86706
TheMexican <mekcukanec42@gmai.com>
2020-11-29 20:25:45 +0300

    FixCardsBackgroundColor

--- app/src/main/java/org/slovenlypolygon/recipes/DishCard.java
+++ app/src/main/java/org/slovenlypolygon/recipes/DishCard.java
@@ -1,15 +1,21 @@
 package org.slovenlypolygon.recipes;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
 import org.slovenlypolygon.recipes.databaseutils.Dish;
 import org.slovenlypolygon.recipes.utils.Converters;
 
 public class DishCard extends CardView {
     private String tag;
     private Dish dish;
+
 
     public DishCard(@NonNull Context context, Dish dish, int index) {
         super(context);
@@ -24,14 +30,15 @@ public class DishCard extends CardView {
         int padding = Converters.fromDPToPixels(context, 100);
 
         ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, layoutHeight);
-
         params.setMargins(layoutMargin, layoutMargin, layoutMargin, layoutMargin);
         params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
         params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
 
         cardView.setLayoutParams(params);
         cardView.setPadding(padding, padding, padding, padding);
-        cardView.setCardBackgroundColor(R.color.searchViewColorWhite);
+        cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.cardBackgroundColorDark));
         cardView.setRadius(Converters.fromDPToPixels(context, 6));
+
+
     }
 }
