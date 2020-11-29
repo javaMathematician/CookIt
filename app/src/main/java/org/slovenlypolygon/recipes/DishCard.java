a2f06bd28016893c08dcb5c495d37714e729962f
TheMexican <mekcukanec42@gmai.com>
2020-11-29 21:31:21 +0300

    TextView + CheckBox

--- app/src/main/java/org/slovenlypolygon/recipes/DishCard.java
+++ app/src/main/java/org/slovenlypolygon/recipes/DishCard.java
@@ -1,8 +1,13 @@
 package org.slovenlypolygon.recipes;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -11,6 +16,10 @@ import androidx.core.content.ContextCompat;
 
 import org.slovenlypolygon.recipes.databaseutils.Dish;
 import org.slovenlypolygon.recipes.utils.Converters;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DishCard extends CardView {
     private String tag;
@@ -39,6 +48,20 @@ public class DishCard extends CardView {
         cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.cardBackgroundColorDark));
         cardView.setRadius(Converters.fromDPToPixels(context, 6));
 
+        //Вставить картинку
 
+        TextView productNameInCards = new TextView(context);
+        productNameInCards.setText(dish.getName().replace("«", "").replace("»", ""));
+        //dish.getName надо заменить на название продута
+        productNameInCards.setTextSize(30);
+        productNameInCards.setTextColor(ContextCompat.getColor(context, R.color.cardTextColor));
+        cardView.addView(productNameInCards);
+
+        //поправить CheckBox
+        CheckBox productSelection = new CheckBox(context);
+        productSelection.setScaleX(2);
+        productSelection.setScaleY(2);
+        productSelection.setBackgroundColor(ContextCompat.getColor(context, R.color.cardTextColor));
+        //cardView.addView(productSelection);
     }
 }
