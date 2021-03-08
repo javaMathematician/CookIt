d3b156e11fb0c6af54624654dd02ac5528f7be84
TheMexican <mekcukanec42@gmai.com>
2021-03-08 19:53:32 +0300

    DrawerLayout NavigationView Side menu

--- app/src/main/java/org/slovenlypolygon/recipes/activities/Ingredients.java
+++ app/src/main/java/org/slovenlypolygon/recipes/activities/Ingredients.java
@@ -3,15 +3,20 @@ package org.slovenlypolygon.recipes.activities;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -33,6 +38,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class Ingredients extends AppCompatActivity {
+    //side menu
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
+    //another
     private List<Dish> dishes;
     private RecyclerView recyclerView;
     private Button changeViewIngredient;
@@ -44,6 +54,29 @@ public class Ingredients extends AppCompatActivity {
         recyclerView = findViewById(R.id.ingredientsRecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //side menu
+        drawerLayout = findViewById(R.id.drawerMain);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_recipe, R.string.open_recipe);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getGroupId();
+                switch (id) {
+                    case R.id.something:
+                        Intent goToSomething = new Intent(Ingredients.this, Dishes.class);
+                        startActivity(goToSomething);
+                        break;
+                }
+                return false;
+            }
+        });
 
         try {
             dishes = Deserializer.deserializeDishes(getResources().openRawResource(R.raw.alpha));
