4be71649e91c9a24697c0fe376eea9514e1b20b0
Seryozha <1460961@gmail.com>
2021-04-11 20:11:22 +0300

    rxjava included))))

--- app/src/androidTest/java/org/slovenlypolygon/recipes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/slovenlypolygon/recipes/ExampleInstrumentedTest.java
@@ -1,8 +1,10 @@
 package org.slovenlypolygon.recipes;
 
 import android.content.Context;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
