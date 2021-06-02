e401c4cd249d226e4d4a12d6981c660d00b6eb61
Seryozha <1460961@gmail.com>
2021-06-02 15:53:27 +0300

    some ocr stuff
    Signed-off-by: Seryozha <1460961@gmail.com>

--- app/src/main/java/org/slovenlypolygon/recipes/MainActivity.java
+++ app/src/main/java/org/slovenlypolygon/recipes/MainActivity.java
@@ -6,23 +6,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.FileProvider;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
+import org.jetbrains.annotations.NotNull;
 import org.slovenlypolygon.recipes.backend.computervision.OCR;
 import org.slovenlypolygon.recipes.backend.database.DataBaseHelper;
 import org.slovenlypolygon.recipes.backend.database.DishComponentDAO;
@@ -35,18 +33,15 @@ import org.slovenlypolygon.recipes.frontend.fragments.dishes.FavoriteDishesFragm
 import org.slovenlypolygon.recipes.frontend.fragments.dishes.RecommendedDishesFragment;
 import org.slovenlypolygon.recipes.frontend.fragments.shoppinglists.ShoppingListFragment;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Objects;
-import java.util.Set;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.SingleObserver;
-import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.aprilapps.easyphotopicker.MediaFile;
+import pl.aprilapps.easyphotopicker.MediaSource;
 
 public class MainActivity extends AppCompatActivity {
     private final static int CAMERA_REQUEST_CODE = 19248;
@@ -56,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private DishComponentDAO dishComponentDao;
     private DrawerLayout drawerLayout;
-    private String realImageFileURI;
+    private EasyImage easyImage;
+    private OCR ocr;
 
     public DishComponentDAO getDishComponentDAO() {
         return dishComponentDao;
@@ -152,50 +148,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runOCR() {
-        try {
-            File imageFile = createImageFile();
-            realImageFileURI = "file://" + imageFile.getAbsolutePath();
+        easyImage = new EasyImage.Builder(getApplicationContext())
+                .setCopyImagesToPublicGalleryFolder(false)
+                .allowMultiple(true)
+                .build();
 
-            Uri imageUri = FileProvider.getUriForFile(MainActivity.this, "org.slovenlypolygon.recipes.fileprovider", imageFile);
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            startActivityForResult(intent, CAMERA_REQUEST_CODE);
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
+        easyImage.openCameraForImage(this);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(realImageFileURI));
+        easyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onMediaFilesPicked(@NotNull MediaFile[] mediaFiles, @NotNull MediaSource mediaSource) {
+                Bitmap bitmap = BitmapFactory.decodeFile(mediaFiles[0].getFile().getAbsolutePath());
 
-                OCR.parseImage(bitmap)
+                if (ocr == null) {
+                    ocr = new OCR();
+                }
+
+                ocr.parseImage(bitmap)
                         .subscribeOn(Schedulers.newThread())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new SingleObserver<Set<String>>() {
-                            @Override
-                            public void onSubscribe(@NonNull Disposable d) {
-                            }
-
-                            @Override
-                            public void onSuccess(@NonNull Set<String> strings) {
-                                System.out.println(strings);
-
-                                getContentResolver().delete(Uri.parse(realImageFileURI), null, null);
-                            }
-
-                            @Override
-                            public void onError(@NonNull Throwable e) {
-                                e.printStackTrace();
-                            }
-                        });
-            } catch (IOException e) {
-                e.printStackTrace();
+                        .subscribe(System.out::println, Throwable::printStackTrace);
             }
-        }
+
+            @Override
+            public void onImagePickerError(@NonNull Throwable error, @NonNull MediaSource source) {
+                error.printStackTrace();
+            }
+
+            @Override
+            public void onCanceled(@NonNull MediaSource source) {
+            }
+        });
+
     }
 
     private void changeComponentView(ComponentType componentType) {
@@ -229,13 +218,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
-    }
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-
-        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
 }
--- app/src/main/java/org/slovenlypolygon/recipes/backend/computervision/OCR.java
+++ app/src/main/java/org/slovenlypolygon/recipes/backend/computervision/OCR.java
@@ -1,6 +1,7 @@
 package org.slovenlypolygon.recipes.backend.computervision;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.util.Base64;
 
@@ -22,50 +23,77 @@ import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 
-import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.Observable;
 
 public class OCR {
-    public static Single<Set<String>> parseImage(Bitmap bitmap) {
-        return Single.create(emitter -> {
+    public Observable<Set<String>> parseImage(Bitmap bitmap) {
+        return Observable.create(emitter -> {
             HttpsURLConnection connection = (HttpsURLConnection) new URL("https://api.ocr.space/parse/image").openConnection();
 
             connection.setRequestMethod("POST");
             connection.setRequestProperty("User-Agent", "Mozilla/5.0");
             connection.setDoOutput(true);
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 75, byteArrayOutputStream);
+            for (int degrees : new int[]{0, 90, 180, 270}) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                scaleBitmap(rotate(bitmap, degrees)).compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
 
-            String string = "data:image/png;base64," + Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-            String encodedQuery = new Uri.Builder()
-                    .appendQueryParameter("apikey", "e9525e2a8b88957")
-                    .appendQueryParameter("language", "rus")
-                    .appendQueryParameter("base64Image", string)
-                    .build().getEncodedQuery();
+                String string = "data:image/png;base64," + Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+                String encodedQuery = new Uri.Builder()
+                        .appendQueryParameter("apikey", "e9525e2a8b88957")
+                        .appendQueryParameter("language", "rus")
+                        .appendQueryParameter("base64Image", string)
+                        .build().getEncodedQuery();
 
-            OutputStream outputStream = connection.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-            writer.write(encodedQuery);
+                OutputStream outputStream = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+                writer.write(encodedQuery);
 
-            writer.flush();
-            writer.close();
-            outputStream.close();
+                writer.flush();
+                writer.close();
+                outputStream.close();
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                JSONObject object = (JSONObject) new JSONObject(IOUtil.toString(reader)).getJSONArray("ParsedResults").get(0);
-                String parsedRaw = object.getString("ParsedText").toLowerCase();
-                String regex = "[а-я]{2,}|[nutela]{3,}";
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    JSONObject object = (JSONObject) new JSONObject(IOUtil.toString(reader)).getJSONArray("ParsedResults").get(0);
+                    String parsedRaw = object.getString("ParsedText").toLowerCase();
+                    String regex = "[а-я]{2,}|[nutela]{3,}";
 
-                Set<String> parsedWords = new HashSet<>();
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(parsedRaw);
+                    Set<String> parsedWords = new HashSet<>();
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(parsedRaw);
 
-                while (matcher.find()) {
-                    parsedWords.add(parsedRaw.substring(matcher.start(), matcher.end()));
+                    while (matcher.find()) {
+                        parsedWords.add(parsedRaw.substring(matcher.start(), matcher.end()));
+                    }
+
+                    emitter.onNext(parsedWords);
                 }
-
-                emitter.onSuccess(parsedWords);
             }
         });
+    }
+
+    private Bitmap scaleBitmap(Bitmap input) {
+        final int currentWidth = input.getWidth();
+        final int currentHeight = input.getHeight();
+        final int currentPixels = currentWidth * currentHeight;
+
+        final long maxPixels = (1024 * 1024) / 4;
+
+        if (currentPixels <= maxPixels) {
+            return input;
+        }
+
+        final double scaleFactor = Math.sqrt(maxPixels / (double) currentPixels);
+        final int newWidthPx = (int) Math.floor(currentWidth * scaleFactor);
+        final int newHeightPx = (int) Math.floor(currentHeight * scaleFactor);
+
+        return Bitmap.createScaledBitmap(input, newWidthPx, newHeightPx, true);
+    }
+
+    private Bitmap rotate(Bitmap source, int degrees) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, false);
     }
 }
